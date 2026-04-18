5fb45344ff6b3bbf155cea7128ed616bd4bdc6b1
betacoder7 <webdesigner2502@gmail.com>
2026-04-18 10:55:51 +0530

    New api add

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 const API_CONFIG = {
   API_BASE_URL: 'https://admin.glyptic.in/api',
   UPLOAD_BASE_URL: 'https://api.glyptic.in',
+  API_SECOUNDBASE_URL: 'https://api.glyptic.in/api'
 };
 
 export default API_CONFIG;
